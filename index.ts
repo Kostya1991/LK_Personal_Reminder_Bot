68f4edd4ae5321b4a16b77bd0ebeecbeb6b7eef7
K.Lysov <K.Lysov@russianpost.ru>
2025-04-25 13:52:42 +0300

    добавил приттер

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import fs from "fs";
-import { Bot, GrammyError, HttpError, InputFile, webhookCallback } from "grammy";
+import {Bot, GrammyError, HttpError, InputFile, webhookCallback} from "grammy";
 
 require("dotenv").config();
 
 const ONE_DAY_TIME = 24 * 60 * 60 * 1000;
 
-const data = fs.readFileSync('./assets/db.json', { encoding: "utf8" });
+const data = fs.readFileSync("./assets/db.json", {encoding: "utf8"});
 const dataObj = JSON.parse(data);
 
 // Создание экземпляра бота
@@ -13,31 +13,37 @@ const bot = new Bot(process.env.BOT_TOKEN!);
 
 /** Добавление меню с командами */
 bot.api.setMyCommands([
-  {
-    command: "start",
-    description: "Запуск бота"
-  },
-  {
-    command: "aboutme",
-    description: "Обо мне"
-  }
+    {
+        command: "start",
+        description: "Запуск бота",
+    },
+    {
+        command: "aboutme",
+        description: "Обо мне",
+    },
 ]);
 
 // Обработка команды /start.
 bot.command("start", async (ctx) => {
-  await ctx.reply(`
+    await ctx.reply(
+        `
       Привет <b>${ctx?.from?.first_name} ${ctx?.from?.last_name}.</b> &#128075; \n Я виртуальный помошник, который будет напоминать тебе о важных событиях за день до их наступления &#128276; \n Все, что тебе нужно - это написать мне сообщение формата: <b>"ДД.ММ.ГГГГ - Сообщение"</b>. \n Но будь внимателен, соблюдение формата - очень важное условие, что бы я мог правильно сохранить данные и своевременно напоминать тебе о наступлении события. \n Если ты ошибешься - я сообщу тебе об этом, или же напишу что событие успешно сохранено! \n Поехали &#128663;
-    `, {
-    parse_mode: "HTML"
-  });
+    `,
+        {
+            parse_mode: "HTML",
+        },
+    );
 });
 
 // Обработка команды /aboutme.
 bot.command("aboutme", async (ctx) => {
-  await ctx.replyWithPhoto(new InputFile("./assets/avatar.jpg"));
-  await ctx.reply(`
+    await ctx.replyWithPhoto(new InputFile("./assets/avatar.jpg"));
+    await ctx.reply(
+        `
     Привет <b>${ctx?.from?.first_name} ${ctx?.from?.last_name}.</b> &#128075; \n Меня зовут Константин, я разработчик данного бота. Если у тебя есть вопросы - ты можешь написать мне в <a href="https://t.me/KonstantinLysov">телеграмм</a>
-    `,{ parse_mode: "HTML", link_preview_options: {is_disabled: true}})
+    `,
+        {parse_mode: "HTML", link_preview_options: {is_disabled: true}},
+    );
 });
 
 // Обработка сообщения от пользователя
@@ -52,7 +58,7 @@ bot.on("message", (ctx) => {
     const data = new Date(date.split(".").reverse().join("-"));
 
     /** Формируем данные для записи в json файл */
-    const payload = { date: data, description };
+    const payload = {date: data, description};
 
     /** Полуачаем из json по ключу пользователя все события */
     const jsonData = dataObj[ctx.from.id] ?? [];
@@ -61,28 +67,29 @@ bot.on("message", (ctx) => {
 
     /** Обновляем файл с данными */
     /** todo: нужно делать асинхронно и отправлять сообщения на успех и если есть ошибка */
-    fs.writeFileSync('./assets/db.json', JSON.stringify(
-      {
-        ...dataObj,
-        [ctx.from.id]: jsonData
-      }
-    ));
+    fs.writeFileSync(
+        "./assets/db.json",
+        JSON.stringify({
+            ...dataObj,
+            [ctx.from.id]: jsonData,
+        }),
+    );
 
-    ctx.reply(`Привет ${ctx.from.first_name} ${ctx.from.last_name}`) // тут доработать сообщение об успехе с смайликом
+    ctx.reply(`Привет ${ctx.from.first_name} ${ctx.from.last_name}`); // тут доработать сообщение об успехе с смайликом
 });
 
 /** Обработка ошибок */
 bot.catch((err) => {
-  const ctx = err.ctx;
-  console.error(`Ошибка при обработке обновления ${ctx.update.update_id}:`);
-  const e = err.error;
-  if (e instanceof GrammyError) {
-    console.error("Ошибка в запросе:", e.description);
-  } else if (e instanceof HttpError) {
-    console.error("Не удалось связаться с Telegram:", e);
-  } else {
-    console.error("Неизвестная ошибка:", e);
-  }
+    const ctx = err.ctx;
+    console.error(`Ошибка при обработке обновления ${ctx.update.update_id}:`);
+    const e = err.error;
+    if (e instanceof GrammyError) {
+        console.error("Ошибка в запросе:", e.description);
+    } else if (e instanceof HttpError) {
+        console.error("Не удалось связаться с Telegram:", e);
+    } else {
+        console.error("Неизвестная ошибка:", e);
+    }
 });
 
 /** Интервал, который раз в сутки проверяет календарь и отправляет сообщение пользователю, если есть событие */
